676eec6cc98bbb6b1787fdcc56a8e0cc28549bf1
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-19 09:12:43 +0000

    tutorial done up to props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Table from './Table';
 
 class App extends Component {
   render() {
+
+    const characters = [
+      {
+               'name': 'Charlie',
+               'job': 'Janitor'
+           },
+           {
+               'name': 'Mac',
+               'job': 'Bouncer'
+           },
+           {
+               'name': 'Dee',
+               'job': 'Aspring actress'
+           },
+           {
+               'name': 'Dennis',
+               'job': 'Bartender'
+           }
+       ];
+
+       const others = 'others'
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='container'>
+        <Table characterData={characters}/>
       </div>
     );
   }
