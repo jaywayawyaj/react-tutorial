7130a9e226533497b8f5f6654b336b5376e658c4
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-19 11:24:30 +0000

    Updates

---
+++ precache-manifest.a5f8ab6fd0a9d4b07544fe852022c256.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "310813581c0b9b1a3eea",
+    "url": "/react-tutorial/static/css/main.38592428.chunk.css"
+  },
+  {
+    "revision": "310813581c0b9b1a3eea",
+    "url": "/react-tutorial/static/js/main.31081358.chunk.js"
+  },
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "1ad83593fd3178ca2fa5",
+    "url": "/react-tutorial/static/js/2.1ad83593.chunk.js"
+  },
+  {
+    "revision": "4c331daa759141533919f26b67615213",
+    "url": "/react-tutorial/index.html"
+  }
+];
